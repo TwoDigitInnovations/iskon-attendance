9c0dc08ea07cf7806a7a8f4c87702c433be0cd7e
Chetan7528 <75465248+Chetan7528@users.noreply.github.com>
2023-07-07 19:54:10 +0530

    minor changge

--- src/app/controller/devotees.js
+++ src/app/controller/devotees.js
@@ -5,63 +5,76 @@ const response = require("./../responses");
 const moment = require("moment");
 
 module.exports = {
-  // saveAttendance: async (req, res) => {
-  //   try {
-  //     const payload = req.body;
-  //     let user;
-  //     let updatedUser = {};
-  //     user = await Devotees.find({
-  //       name: payload.name,
-  //       phone: payload.phone,
-  //     }).lean();
+  saveAttendance: async (req, res) => {
+    try {
+      const payload = req.body;
+      let user;
+      let updatedUser = {};
 
-  //     console.log(user);
-  //     let userDetail = {
-  //       phone: payload.phone,
-  //       name: payload.name,
-  //       address: payload.address || "",
-  //       occupation: payload.occupation || "",
-  //       marital_status: payload.marital_status,
-  //       user_id: payload.user_id,
-  //     };
-  //     if (user.length) {
-  //       let attendance = new Attendance({
-  //         devotee_id: user[0]._id,
-  //         user_id: payload.user_id,
-  //         attendance_date: payload.date,
-  //       });
-  //       const at = await attendance.save();
-  //       const dev = await Devotees.findByIdAndUpdate(user[0]._id, userDetail, {
-  //         new: true,
-  //         upsert: true,
-  //       });
-  //       updatedUser = {
-  //         devotees: dev,
-  //         attendance: at,
-  //       };
-  //       return response.created(res, updatedUser);
-  //     } else {
-  //       let devotees = new Devotees(userDetail);
-  //       const dev = await devotees.save();
-  //       console.log("dev===========>", dev);
-  //       if (dev) {
-  //         let attendance = new Attendance({
-  //           devotee_id: dev._id,
-  //           user_id: dev.user_id,
-  //           attendance_date: payload.date,
-  //         });
-  //         const at = await attendance.save();
-  //         updatedUser = {
-  //           devotees: dev,
-  //           attendance: at,
-  //         };
-  //         return response.created(res, updatedUser);
-  //       }
-  //     }
-  //   } catch (error) {
-  //     return { message: "err" };
-  //   }
-  // },
+      let devotee = [];
+      user = await Devotees.find({
+        name: payload.name,
+        phone: payload.phone,
+      }).lean();
+      devotee = await Attendance.find({
+        devotee_id: user[0]._id,
+        attendance_date: payload.date,
+      }).lean();
+
+      if (devotee.length > 0) {
+        return response.error(res, {
+          name: "Error",
+          message: "Attendace Already Saved",
+          stack: "",
+        });
+      }
+      console.log(user);
+      let userDetail = {
+        phone: payload.phone,
+        name: payload.name,
+        address: payload.address || "",
+        occupation: payload.occupation || "",
+        marital_status: payload.marital_status,
+        user_id: payload.user_id,
+      };
+      if (user.length) {
+        let attendance = new Attendance({
+          devotee_id: user[0]._id,
+          user_id: payload.user_id,
+          attendance_date: payload.date,
+        });
+        const at = await attendance.save();
+        const dev = await Devotees.findByIdAndUpdate(user[0]._id, userDetail, {
+          new: true,
+          upsert: true,
+        });
+        updatedUser = {
+          devotees: dev,
+          attendance: at,
+        };
+        return response.created(res, updatedUser);
+      } else {
+        let devotees = new Devotees(userDetail);
+        const dev = await devotees.save();
+        console.log("dev===========>", dev);
+        if (dev) {
+          let attendance = new Attendance({
+            devotee_id: dev._id,
+            user_id: dev.user_id,
+            attendance_date: payload.date,
+          });
+          const at = await attendance.save();
+          updatedUser = {
+            devotees: dev,
+            attendance: at,
+          };
+          return response.created(res, updatedUser);
+        }
+      }
+    } catch (error) {
+      return { message: "err" };
+    }
+  },
 
   saveAttendance: async (req, res) => {
     try {
